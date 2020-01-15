7b63d9f2e921e71e2f6b8ba7b30694c834e5283f
Luiz.Uliana <luizeduardr@gmail.com>
2020-01-15 00:13:22 -0300

    rotas da aplicação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,6 @@ const routes = Router();
 routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store );
 
-routes.get('/search', SearchController.index)
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
