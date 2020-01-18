8f454132329b3113182a5b0eae4797a5815c43b8
Luiz.Uliana <luizeduardr@gmail.com>
2020-01-18 01:11:00 -0300

    Pagina de perfil

--- DevRadar/src/pages/Profile.js
+++ DevRadar/src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { WebView } from 'react-native-webview'
+import { WebView } from 'react-native-webview';
 
 
 function Profile({ navigation }) {
