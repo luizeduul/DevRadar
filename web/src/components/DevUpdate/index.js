0ffaef09885f3199c08e047773a120d5f0e7b6a8
Luiz.Uliana <luizeduardr@gmail.com>
2020-02-03 13:57:06 -0300

    Alterações de dependencias e anotações

--- web/src/components/DevUpdate/index.js
+++ web/src/components/DevUpdate/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
-//import api from '../../services/api';
+//import api from '../../services/api'; fix issue later
 import Axios from "axios";
 
 import './styles.css'
